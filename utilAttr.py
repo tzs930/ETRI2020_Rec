95162400661119983a95e50004d7d96b6dfd914c
gr8joo <hjhwang@ai.kaist.ac.kr>
2020-11-29 01:24:46 +0900

    Initial commit

---
+++ utilAttr.py
@@ -0,0 +1,146 @@
+import sys
+import copy
+import random
+import numpy as np
+from collections import defaultdict
+
+
+def data_partition(fname):
+    usernum = 0
+    itemnum = 0
+    User = defaultdict(list)
+    user_train = {}
+    user_valid = {}
+    user_test = {}
+    # assume user/item index starting from 1
+    f = open('data/%s.txt' % fname, 'r')
+    for line in f:
+        u, i = line.rstrip().split(' ')
+        u = int(u)
+        i = int(i)
+        usernum = max(u, usernum)
+        itemnum = max(i, itemnum)
+        User[u].append(i)
+
+    for user in User:
+        nfeedback = len(User[user])
+        if nfeedback < 3:
+            user_train[user] = User[user]
+            user_valid[user] = []
+            user_test[user] = []
+        else:
+            user_train[user] = User[user][:-2]
+            user_valid[user] = []
+            user_valid[user].append(User[user][-2])
+            user_test[user] = []
+            user_test[user].append(User[user][-1])
+    return [user_train, user_valid, user_test, usernum, itemnum]
+
+
+def evaluate(model, dataset, args, sess):
+    [train, valid, test, usernum, itemnum] = copy.deepcopy(dataset)
+    attr = np.load('data/genome_mat.npy')
+
+    NDCG = 0.0
+    HT = 0.0
+    valid_user = 0.0
+
+    if usernum>10000:
+        users = random.sample(xrange(1, usernum + 1), 10000)
+    else:
+        users = xrange(1, usernum + 1)
+
+    for u in users:
+        if len(train[u]) < 1 or len(test[u]) < 1: continue
+
+        seq = np.zeros([args.maxlen], dtype=np.int32)
+        seq_attr = np.zeros([args.maxlen, 1128], dtype=np.float32)
+
+        idx = args.maxlen - 1
+        seq[idx] = valid[u][0]
+        seq_attr[idx] = attr[seq[idx]]
+
+        idx -= 1
+        for i in reversed(train[u]):
+            seq[idx] = i
+            seq_attr[idx] = attr[seq[idx]]
+            idx -= 1
+            if idx == -1: break
+
+        rated = set(train[u])
+        rated.add(0)
+        item_idx = [test[u][0]]
+        item_idx_attr = [ attr[item_idx[0]] ]
+        for _ in range(100):
+            t = np.random.randint(1, itemnum + 1)
+            while t in rated: t = np.random.randint(1, itemnum + 1)
+            item_idx.append(t)
+            item_idx_attr.append( attr[t] )
+
+
+        predictions = -model.predict(sess, [u], [seq], item_idx, [seq_attr], item_idx_attr)
+        predictions = predictions[0]
+
+        rank = predictions.argsort().argsort()[0]
+
+        valid_user += 1
+
+        if rank < 10:
+            NDCG += 1 / np.log2(rank + 2)
+            HT += 1
+        if valid_user % 100 == 0:
+            print '.',
+            sys.stdout.flush()
+
+    return NDCG / valid_user, HT / valid_user
+
+
+def evaluate_valid(model, dataset, args, sess):
+    [train, valid, test, usernum, itemnum] = copy.deepcopy(dataset)
+    attr = np.load('data/genome_mat.npy')
+
+    NDCG = 0.0
+    valid_user = 0.0
+    HT = 0.0
+    if usernum>10000:
+        users = random.sample(xrange(1, usernum + 1), 10000)
+    else:
+        users = xrange(1, usernum + 1)
+    for u in users:
+        if len(train[u]) < 1 or len(valid[u]) < 1: continue
+
+        seq = np.zeros([args.maxlen], dtype=np.int32)
+        seq_attr = np.zeros([args.maxlen, 1128], dtype=np.float32)
+        idx = args.maxlen - 1
+        for i in reversed(train[u]):
+            seq[idx] = i
+            seq_attr[idx] = attr[ seq[idx] ]
+            idx -= 1
+            if idx == -1: break
+
+        rated = set(train[u])
+        rated.add(0)
+        item_idx = [valid[u][0]]
+        item_idx_attr = [ attr[item_idx[0]] ]
+
+        for _ in range(100):
+            t = np.random.randint(1, itemnum + 1)
+            while t in rated: t = np.random.randint(1, itemnum + 1)
+            item_idx.append(t)
+            item_idx_attr.append( attr[t] )
+
+        predictions = -model.predict(sess, [u], [seq], item_idx, [seq_attr], item_idx_attr)
+        predictions = predictions[0]
+
+        rank = predictions.argsort().argsort()[0]
+
+        valid_user += 1
+
+        if rank < 10:
+            NDCG += 1 / np.log2(rank + 2)
+            HT += 1
+        if valid_user % 100 == 0:
+            print '.',
+            sys.stdout.flush()
+
+    return NDCG / valid_user, HT / valid_user
